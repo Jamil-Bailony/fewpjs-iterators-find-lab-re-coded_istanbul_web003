3e920fadcbdec8baf7bec5c996c85f7aca7f5c2c
Jamil Bailony <jBailony@outlook.com>
2020-08-23 19:59:03 +0300

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(records) {
+  const record = records.find(({result}) => result === 'W');
+
+  return !!record ? record.year:undefined;
+}
